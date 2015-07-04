8330ba3269f3ff9c97c15e408f5b214bf7717be9
Toby Flemming <tobyf93@hotmail.com>
2015-07-04 14:30:16 +1000

    Change csv_to_db.rb to handle incomes.csv

--- csv_to_db.rb
+++ csv_to_db.rb
@@ -4,6 +4,13 @@ ARGF.each_with_index do |line, i|
   unless i == 0
     line.strip!
     values = line.split(',')
-    puts "PropertyPrice.create :date => '#{values[0]}', :region => '#{values[1]}', :median => '#{values[2]}'"
+
+    gender = values[0]
+    state = values[1]
+    age = values[2]
+    average = values[3]
+
+    matches = age.match(/(Under (?<max>18)|(?<min>\d+) \- (?<max>\d+)|(?<min>75) and over)/)
+    puts "min: #{matches['min']} max: #{matches['max']}"
   end
 end
