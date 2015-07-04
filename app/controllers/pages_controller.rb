7887a1c36656ed73dae29df3655bb5813b8c2ccf
ReenaRajani <reenahrajani@gmail.com>
2015-07-04 16:43:05 +1000

    seed problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,19 @@ class PagesController < ApplicationController
   def landing
   end
   def details
+
+    property = PropertyPrice.where("region = '#{params[:details][:region]}'").order(date: :desc).first
+    binding.pry
+    # set = PropertyPrice.find_by (region: params[:region] and EXTRACT(year from date) = 2015;
+    price = property.median
+    # set1 = Income.where()
+    # avg_income = Income.find_by 
+
+
     info = {
       :success => true
     }
     render :text => info.to_json
   end
+
 end
